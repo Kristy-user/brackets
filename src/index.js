a04ce3d32dae1f120217456821b2792d2aa806b0
Kristina_Klepik <kristygomel@gmail.com>
2022-07-13 08:50:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsObj = Object.fromEntries(bracketsConfig);
+  let resultArr = [];
+  str.split('').forEach((item) => {
+    let lastElem = resultArr[resultArr.length - 1];
+    if (resultArr.length > 0 && bracketsObj[lastElem] === item) {
+      resultArr.pop();
+    } else {
+      resultArr.push(item);
+    }
+  });
+  return resultArr.length === 0;
+};
